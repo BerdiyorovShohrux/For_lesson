4a46ebc0649523d12907376cfa5c6cb1adace8dd
Berdiyorov Shohrux <berdiyorovshohrux7@gmail.com>
2026-02-25 23:35:41 +0500

    Add multiple print statements to python.py

--- python.py
+++ python.py
@@ -1,1 +1,24 @@
+print("Hello, World!")
 
+print("Shohrux")
+
+print("Kuch Adolatda.")
+
+print("Salom.\nXayr.")
+
+print('"Python is very interesting"')
+
+print("Gemini")
+print("2 yosh")
+
+print(2026)
+
+print("Mening ismim Shohrux va hozir 2026-yil")
+
+print("salom")
+print() 
+print("xayr")
+
+print("""Shohrux,
+Bexruz,
+Sardor.""")
