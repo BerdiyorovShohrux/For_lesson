f07f014dd81179442b2442fb1f511e11b1d032b4
Berdiyorov Shohrux <berdiyorovshohrux7@gmail.com>
2026-02-25 23:46:14 +0500

    Update python3.py

--- python3.py
+++ python3.py
@@ -1,1 +1,32 @@
-
+  ism = input("Ismingizni kiriting: ")
+  print(ism)
+  
+  rang = input("Sevimli rangingiz qaysi? ")
+  print("Sizning sevimli rangingiz " + rang)
+  
+  son1 = int(input("Birinchi sonni kiriting: "))
+  son2 = int(input("Ikkinchi sonni kiriting: "))
+  print("Yig'indi:", son1 + son2)
+  
+  yosh = input("Yoshingiz nechada? ")
+  print("Siz " + yosh + " yoshdasiz")
+  
+  taom = input("Sevimli taomingiz nima? ")
+  print(f"{taom} juda mazali taom")
+  
+  boy = input("Bo'yingiz necha sm? ")
+  print("Sizning bo'yingiz: " + boy + " sm")
+  
+  t_yil = input("Tug'ilgan yilingizni kiriting: ")
+  print("Siz " + t_yil + "-yilda tug'ilgansiz")
+  
+  f_ism = input("Ismingizni kiriting: ")
+  familiya = input("Familiyangizni kiriting: ")
+  print("To'liq ism:", f_ism, familiya)
+  
+  hayvon = input("Uy hayvoningizning nomi nima? ")
+  print(hayvon + " juda aqlli hayvon bo'lsa kerak!")
+  
+  raqam = int(input("Biror raqam kiriting: "))
+  natija = raqam * 10
+  print(natija)
