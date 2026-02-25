abd27125e304249c0aa2dcc71182fd3d1beec3ab
Berdiyorov Shohrux <berdiyorovshohrux7@gmail.com>
2026-02-25 23:57:30 +0500

    Create python5.py

---
+++ python5.py
@@ -0,0 +1,36 @@
+yosh = int(input("Yoshingizni kiriting: "))
+print(yosh)
+print(type(yosh))
+
+son1 = int(input("Birinchi sonni kiriting: "))
+son2 = int(input("Ikkinchi sonni kiriting: "))
+yigindi = son1 + son2
+print("Natija:", yigindi, type(yigindi))
+
+
+kasrson = float(input("Kasr son kiriting (masalan 3.5): "))
+print("Qiymat:", kasrson, type(kasrson))
+
+matn = input("Biror gap yozing: ")
+print(type(matn))
+
+ism = input("Ismingiz nima? ")
+yosh = input("Yoshingiz nechada? ")
+print(f"Salom {ism}, siz {yosh_input} yoshdasiz.")
+
+raqam = input("Butun son kiriting: ")
+kasrga = float(raqam)
+print(kasrga)
+
+son= int(input("Raqam kiriting: "))
+natija = son * 2
+print(natija, type(natija))
+
+kasr_son = float(input("Kasr son kiriting: "))
+print("Float shakli:", k_son)
+print("Int shakli", int(k_son))
+  
+a = input("A sonini kiriting: ")
+b = input("B sonini kiriting: ")
+jami = int(a) + int(b)
+print(jami, type(jami))
