e11383e97c51a291d7b6c5aeb4f93e8171157c88
Berdiyorov Shohrux <berdiyorovshohrux7@gmail.com>
2026-02-25 23:51:34 +0500

    Create python4.py

---
+++ python4.py
@@ -0,0 +1,35 @@
+son = 25
+print("Qiymat:", son,)
+print(type(son))
+
+kasr = 3.14
+print("Kasr son:", kasr)
+
+matn = "Salom Python"
+print(type(matn))
+
+m = True
+print("Turi:", type(m))
+
+yosh = 20
+ism = "Shoh"
+print(ism, yosh)
+
+a, b, c, d = 10, 5.5, "Dars", False
+print(type(a), type(b), type(c), type(d))
+
+narx = 12500.50
+print("Qiymat:", narx)
+print(type(narx))
+
+butun_son = 100
+kasrga = float(butun_son)
+print(kasrga)
+
+matn = "123"
+print(type(matn))
+
+holat = False
+print("avvalgi holat:", holat)
+holat = True
+print("keyingi holat:", holat)
