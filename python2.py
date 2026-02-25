49d3a934b2b6a6907772e9b25a721612bc9205ce
Berdiyorov Shohrux <berdiyorovshohrux7@gmail.com>
2026-02-25 23:40:56 +0500

    Update and rename python2 to python2.py

---
+++ python2.py
@@ -0,0 +1,34 @@
+
+name = "Shohrux"
+print(name)
+
+age = 25
+height = 65
+print(age, height)
+
+city = "Jizzax"
+print(city)
+
+yil = 2026
+print(yil)
+
+Sevimli_raqam = 7
+print(Sevimli_raqam)
+
+a = 15
+b = 25
+print(a + b)
+
+name = "Shohrux"
+print("Salom, " + my_name +"!")
+
+x = 10
+print("Eski qiymat:", x)
+x = 20 
+print("Yangi qiymat:", x)
+
+x, y, z = 5, 10, 15
+print(x, y, z)
+
+Python_language = True
+print(Python_language)
