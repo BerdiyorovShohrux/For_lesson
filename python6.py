4117fa42f2b08d30a181f955e29fe28e4a53e3de
Berdiyorov Shohrux <berdiyorovshohrux7@gmail.com>
2026-02-26 09:40:12 +0500

    Add various input checks and conditions in python6.py

---
+++ python6.py
@@ -0,0 +1,71 @@
+son = float(input("Raqam kiriting: "))
+if son > 0:
+    print("Musbat")
+elif son < 0:
+    print("Manfiy")
+else:
+    print("Nol")
+
+yosh = int(input("Yoshingizni kiriting: "))
+if yosh >= 18:
+    print("Siz kattasiz")
+else:
+    print("Siz voyaga yetmagansiz")
+
+son = int(input("Butun son kiriting: "))
+if son % 2 == 0:
+    print("Juft son")
+else:
+    print("Toq son")
+
+baho = int(input("Bahoni kiriting: "))
+if baho >= 50:
+    print("Imtihondan otdingiz")
+else:
+    print("Imtihondan otmadiz")
+
+son = int(input("Son kiriting: "))
+if son % 2 == 0 and son % 3 == 0:
+    print("2ga ham 3ga ham bo'linadi")
+else:
+    print("To'gri son kirit")
+
+gradus = float(input("Haroratni kiriting: "))
+if gradus > 30:
+    print("Issiq")
+elif gradus < 10:
+    print("Sovuq")
+else:
+    print("Iliq")
+
+
+s1 = float(input("Birinchi son: "))
+s2 = float(input("Ikkinchi son: "))
+if s1 > s2:
+    print("Kattasi:", s1)
+elif s2 > s1:
+    print("Kattasi:", s2)
+else:
+    print("Ular teng")
+
+soz = input("Parolni kiriting: ")
+if soz == "Python":
+    print("To'g'ri!")
+else:
+    print("Qayta urinib ko'ring!")
+
+kun = input("Hafta kunini kiriting: ").lower()
+if kun == "dushanba":
+    print("Hafta boshi")
+elif kun == "seshanba":
+    print("O'qish kuni")
+elif kun == "chorshanba":
+    print("Hafta o'rtasi")
+elif kun == "payshanba":
+    print("Kino kuni")
+elif kun == "juma":
+    print("Muborak kun")
+elif kun == "shanba" or kun == "yakshanba":
+    print("Dam olish kuni")
+else:
+    print("Bunday hafta kuni yo'q")
